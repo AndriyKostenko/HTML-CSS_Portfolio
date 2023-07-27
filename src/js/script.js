4085f92aaf09d8a6bfcaae6fafc5a82c1341096e
Andriy Kostenko <a.kostenkouk@gmail.com>
2023-07-27 12:06:44 -0400

    completed funct. for sending msgs

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,36 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       close = document.querySelector('.menu__close');
-
-
       
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-});
 
-close.addEventListener('click', () => {
-    menu.classList.remove('active');
-})
+ 
+function open_close_menu(hamburger, close, menu) {
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
+    })
+
+    close.addEventListener('click', () => {
+        menu.classList.remove('active');
+    })
+}
+    
+
+window.onload = function() {
+    const form = document.getElementById('contact_form');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        emailjs.sendForm('service_w2i0wjs', 'template_s65z8tw', form)
+            .then(function() {
+                alert('Message has been sent!');
+                form.reset();
+            }, function(error) {
+                alert("Oops, error has occured...please try a different way.");
+                form.reset();
+            });
+    });
+}
 
 
-
-
+open_close_menu(hamburger, close, menu);
